7218e4859d0a0e85772202952edc75c7ce915c92
deaaprizal <dea.afrizal@icloud.com>
2022-05-10 03:51:04 +0700

    renaming

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css"
 import NavigationBar from "./components/NavigationBar"
 import Intro from "./components/Intro"
-import Trending from "./components/Trending"
+import DiscoverMovies from "./components/discover/movies"
 
 import "./style/landingPage.css"
 
@@ -14,7 +14,7 @@ function App() {
       </div>
 
       <div className="trending">
-        <Trending />
+        <DiscoverMovies />
       </div>
     </div>
   )
